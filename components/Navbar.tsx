27fbbb07144be734cd25db486a75ef99e66790ae
Gabriel <gabrielgomezrojas0501@gmail.com>
2025-06-06 19:39:35 -0500

    feat: completed base navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function Navbar(){
     return (
-        <nav>
-            <h1>Task Manager</h1>
+        <nav className='bg-gray-800 p-4 text-white flex justify-between items-center'>
+            <h1 className='text-xl font-bold'>Task Manager</h1>
+
+            <div className='flex space-x-4'>
+                <Link href="/dashboard" className='hover:text-gray-300'>Dashboard</Link>
+                <Link href="/login" className='hover:text-gray-300'>Login</Link>
+                <Link href="/register" className='hover:text-gray-300'>Register</Link>
+            </div>
         </nav>
     )
 }

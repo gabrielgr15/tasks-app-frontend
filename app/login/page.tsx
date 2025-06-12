8c48d30bb92cf0cd52269032bd7514d39631a270
Gabriel <gabrielgomezrojas0501@gmail.com>
2025-06-12 13:15:36 -0500

    feat: Add working login flow

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,65 @@
+'use client';
 import Container from "@/components/Container";
+import { useAuth } from "@/context/Authcontext";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 import Input from "@/components/Input";
 import Button from "@/components/Button";
 
+
+
 export default function LoginPage() {
-    return (
-      <Container>
-        <h1 className="text-2xl font-bold mb-4">Login</h1>
-        <Input type="email" placeholder="Email address" />
-        <Input type="password" placeholder="Password" className="mt-4" />
-        <Button className="mt-6">
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { login } = useAuth();
+  const router = useRouter();
+
+  const handleSubmit = async (event : React.FormEvent) => {
+    event.preventDefault();
+    try{
+      const response = await fetch('task-app-api-gateway-production.up.railway.app/api/users/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      })
+      const data = await response.json()
+
+      if (response.ok) {
+        localStorage.setItem('authToken', data.accessToken)
+        login(email, password);
+        router.push('/dashboard');
+      }
+      else {
+        
+      }  
+    }catch (error){
+      
+    }
+  }
+
+  return (
+    <Container>
+      <h1 className="text-2xl font-bold mb-4">Login</h1>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <Input
+          type="email"
+          placeholder="Email address"
+          value = {email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value = {password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button type="submit">
           Login
         </Button>
-      </Container>
-    );
-  }
+      </form>
+    </Container>
+  );
+}
